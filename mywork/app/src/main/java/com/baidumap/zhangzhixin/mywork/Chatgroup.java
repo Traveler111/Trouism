32fe16b47be7aedec41c618b6dd1327526e61c44
zzx <1046044020@qq.com>
2016-11-23 09:13:42 +0800

    关于环信的单聊demo

---
+++ mywork/app/src/main/java/com/baidumap/zhangzhixin/mywork/Chatgroup.java
@@ -0,0 +1,15 @@
+package com.baidumap.zhangzhixin.mywork;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhangzhixin on 2016/11/22.
+ */
+public class Chatgroup extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
